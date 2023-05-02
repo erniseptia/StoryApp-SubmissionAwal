dd9512e45644d86474c239d1727db09be665b00e
erniseptia <92901309+erniseptia@users.noreply.github.com>
2023-05-02 17:35:51 -0600

    update

---
+++ app/src/main/java/app/story/mystoryappneww/utils/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+package app.story.mystoryappneww.utils
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import app.story.mystoryappneww.login.LoginViewModel
+
+class ViewModelFactory(private val pref: UserPreference) : ViewModelProvider.NewInstanceFactory()  {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(AddStoryViewModel::class.java) -> {
+                AddStoryViewModel(pref) as T
+            }
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(pref) as T
+            }
+            modelClass.isAssignableFrom(RegisterViewModel::class.java) -> {
+                RegisterViewModel() as T
+            }
+            modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
+                LoginViewModel(pref) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+}
